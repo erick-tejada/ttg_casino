3e11e55ece25e574aaaeec47a364e94d27b11243
Erick Tejada <erick.tejada@tejadatech.com>
2023-03-31 23:32:16 +0000

    [FIX] added  force_save="1" to form field

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'TTG - Casino',
     'author': "Tejada Tech Group EIRL",
-    'version': '15.0.1.0',
+    'version': '15.0.1.1',
     'sequence': 100,
     'category': 'Accounting/Accounting',
     'description': """
