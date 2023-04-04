7df955ed2e254c7c40dc23d4fc5b4e4f07e27738
Erick Tejada <erick.tejada@tejadatech.com>
2023-04-04 22:15:34 +0000

    [IMP] Update version

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'TTG - Casino',
     'author': "Tejada Tech Group EIRL",
-    'version': '15.0.1.1',
+    'version': '15.0.1.2',
     'sequence': 100,
     'category': 'Accounting/Accounting',
     'description': """
