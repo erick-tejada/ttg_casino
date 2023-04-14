f21bce51ce02e1b72235d4c58e0d212316e7271b
Erick Tejada <erick.tejada@tejadatech.com>
2023-04-14 16:34:07 +0000

    [IMP] Domain sobrante faltante

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'TTG - Casino',
     'author': "Tejada Tech Group EIRL",
-    'version': '15.0.2.2',
+    'version': '15.0.2.3',
     'sequence': 100,
     'category': 'Accounting/Accounting',
     'description': """
