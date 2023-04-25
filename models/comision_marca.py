3f617a3e5e23a7e588eaf385850860d8176a1869
Erick Tejada <erick.tejada@tejadatech.com>
2023-04-25 13:46:03 +0000

    [IMP] Reporte de Comision de Marcas con maquinas

--- models/comision_marca.py
+++ models/comision_marca.py
@@ -35,7 +35,7 @@ class ComisionMarca(models.Model):
         ('draft', 'Borrador'),
         ('done', 'Cerrado'),
         ], string='Estado', readonly=True, copy=False, index=True, tracking=3, default='draft')
-    name = fields.Char(string='Nombre', required=True, copy=False, readonly=True, index=True, default=lambda self: 'Nuevo')
+    name = fields.Char(string='Nombre', required=True, copy=False, tracking=3, readonly=True, index=True, default=lambda self: 'Nuevo')
     company_id = fields.Many2one('res.company', string='Compañía', required=True, default=lambda self: self.env.company)
     currency_id = fields.Many2one('res.currency', string='Moneda', related='company_id.currency_id', store=True)
     month = fields.Selection([
@@ -57,10 +57,10 @@ class ComisionMarca(models.Model):
         ('2', '2da Quincena'),
         ], string='Quincena', required=True, copy=False, index=True, tracking=3, default=_default_quincena)
     year = fields.Integer('Año', required=True, default=_default_year)
-    lender_partner_id = fields.Many2one('res.partner', string="Prestamista", required=True, domain="[('x_is_lender', '=', True)]")
+    lender_partner_id = fields.Many2one('res.partner', string="Prestamista", tracking=3, required=True, domain="[('x_is_lender', '=', True)]")
     note = fields.Text('Notas')
 
-    amount = fields.Monetary('Monto a Pagar', readonly=True)
+    amount = fields.Monetary('Monto a Pagar', tracking=3, readonly=True)
     line_ids = fields.One2many('casino.comision.marca.linea', 'comision_id', string='Detalle de Comisiones')
 
     @api.constrains('company_id', 'year', 'month', 'lender_partner_id')
@@ -116,19 +116,42 @@ class ComisionMarca(models.Model):
             else:
                 date_end = '%s-%s-01' % (record.year, str(int(record.month) + 1).zfill(2)) # same_year-next_month-01
 
-        # get all
-        marca_ids = self.env['casino.marca.mesa'].search([
+        marcas_domain = [
             ('company_id','=',record.company_id.id),
             ('date','>=',date_start),
             ('date','<',date_end),
             ('lender_partner_id','=',record.lender_partner_id.id),
             #('state','=','done'),
-        ])
+        ]
+        
+        # MESAS
+        marca_ids = self.env['casino.marca.mesa'].search(marcas_domain)
 
-        totals_by_date = {}
+        totals_mesas_by_date = {}
         for marca in marca_ids:
             date_key = marca.date.strftime(DF)
-            totals_by_date[date_key] = totals_by_date[date_key] + marca.amount if date_key in totals_by_date else marca.amount
+            if date_key in totals_mesas_by_date:
+                totals_mesas_by_date[date_key]['amount_mesas'] = totals_mesas_by_date[date_key]['amount_mesas'] + marca.amount
+            else:
+                totals_mesas_by_date[date_key] = {'amount_mesas': marca.amount, 'amount_maquinas': 0.0, 'retencion_maquina': 0}
+        
+        # MAQUINAS
+        marca_ids = self.env['casino.marca.maquina'].search(marcas_domain)
+
+        for marca in marca_ids:
+            date_key = marca.date.strftime(DF)
+            if date_key in totals_mesas_by_date:
+                totals_mesas_by_date[date_key] = {
+                    'amount_mesas': totals_mesas_by_date[date_key]['amount_mesas'],
+                    'amount_maquinas': totals_mesas_by_date[date_key]['amount_maquinas'] + marca.amount,
+                    'retencion_maquina': marca.cuadre_id.retencion_maquina,
+                }
+            else:
+                totals_mesas_by_date[date_key] = {
+                    'amount_mesas': 0.0, 
+                    'amount_maquinas': marca.amount, 
+                    'retencion_maquina': marca.cuadre_id.retencion_maquina
+                }
         
         # Calculate Fee
         x_tier1_percent = record.lender_partner_id.x_tier1_percent
@@ -136,17 +159,28 @@ class ComisionMarca(models.Model):
         x_tier2_amount = record.lender_partner_id.x_tier2_amount
 
         total = 0
-        for date, amount in totals_by_date.items():
-            percent_to_use = x_tier2_percent if (x_tier2_amount > 0) and (amount > x_tier2_amount) else x_tier1_percent
-            amount_fee = amount * percent_to_use
+        for date, vals in totals_mesas_by_date.items():
+            # MESAS
+            mesas_percent_to_use = x_tier2_percent if (x_tier2_amount > 0) and (vals['amount_mesas'] > x_tier2_amount) else x_tier1_percent
+            mesas_amount_fee = vals['amount_mesas'] * mesas_percent_to_use
+
+            # MAQUINAS
+            maquinas_percent_to_use = x_tier2_percent if (x_tier2_amount > 0) and (vals['amount_maquinas'] > x_tier2_amount) else x_tier1_percent
+            maquinas_amount_fee = vals['amount_maquinas'] * (vals['retencion_maquina'] / 100) * maquinas_percent_to_use
+            
             self.env['casino.comision.marca.linea'].create({
                 'comision_id': record.id,
                 'date': date,
-                'amount_marcas': amount,
-                'amount_fee': amount_fee,
-                'amount_percent': percent_to_use,
+                'amount_marcas': vals['amount_mesas'],
+                'amount_fee': mesas_amount_fee,
+                'amount_percent': mesas_percent_to_use,
+
+                'amount_marcas_maquinas': vals['amount_maquinas'],
+                'amount_fee_maquinas': maquinas_amount_fee,
+                'retencion_maquina': vals['retencion_maquina'] / 100,
+                'amount_percent_maquinas': maquinas_percent_to_use,
             })
-            total += amount_fee
+            total += mesas_amount_fee + maquinas_amount_fee
         
         record.amount = total
 
@@ -163,6 +197,11 @@ class ComisionMarcaLinea(models.Model):
     state = fields.Selection(related='comision_id.state', store=True)
 
     date = fields.Date('Fecha')
-    amount_marcas = fields.Monetary('Monto Marcas')
-    amount_fee = fields.Monetary('Monto Comision')
-    amount_percent = fields.Float('Porciento de Comision')
+    amount_marcas = fields.Monetary('Monto Marcas Mesas')
+    amount_fee = fields.Monetary('Monto Comision Mesas')
+    amount_percent = fields.Float('Porciento de Comision Mesas')
+
+    amount_marcas_maquinas = fields.Monetary('Monto Marcas Máquinas')
+    amount_fee_maquinas = fields.Monetary('Monto Comision Máquinas')
+    retencion_maquina = fields.Float('Retención Máquinas')
+    amount_percent_maquinas = fields.Float('Porciento de Comision Máquinas')
