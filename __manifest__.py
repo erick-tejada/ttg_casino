f354d9a9d72e387995ea54970190544d3c9058fc
Erick Tejada <erick.tejada@tejadatech.com>
2023-12-18 10:22:49 -0400

    [IMP] cobros TC readonly when done

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'TTG - Casino',
     'author': "Tejada Tech Group EIRL",
-    'version': '15.0.2.15',
+    'version': '15.0.2.16',
     'sequence': 100,
     'category': 'Accounting/Accounting',
     'description': """
