ba525e571498a5d674411e177c58fb77827abc44
Erick Tejada <erick.tejada@tejadatech.com>
2024-03-14 14:22:02 -0400

    [FIX] removed code from view

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'TTG - Casino',
     'author': "Tejada Tech Group EIRL",
-    'version': '15.0.3.3',
+    'version': '15.0.3.4',
     'sequence': 100,
     'category': 'Accounting/Accounting',
     'description': """
