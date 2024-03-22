3a902c7cd02eb1312481b63fcdd267665953eb90
Erick Tejada <erick.tejada@tejadatech.com>
2024-03-22 11:08:41 -0400

    [IMP] column order change

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'TTG - Casino',
     'author': "Tejada Tech Group EIRL",
-    'version': '15.0.3.10',
+    'version': '15.0.3.11',
     'sequence': 100,
     'category': 'Accounting/Accounting',
     'description': """
