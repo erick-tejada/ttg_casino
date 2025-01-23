ec9c17d734175657662e5cc1ddf4a79140849a79
Erick Tejada <erick.tejada@tejadatech.com>
2025-01-23 09:52:31 -0400

    [IMP] deposito DOP ahora muestra negativo si no tuvo suficiente

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'TTG - Casino',
     'author': "Tejada Tech Group EIRL",
-    'version': '15.0.3.13',
+    'version': '15.0.3.14',
     'sequence': 100,
     'category': 'Accounting/Accounting',
     'description': """
