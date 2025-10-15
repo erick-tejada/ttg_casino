85c6ad1450791361ddf32e0164c28a73a584fb75
Conti <conty1505@gmail.com>
2025-10-15 19:34:50 -0300

    [IMP]Update version 18.0

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'TTG - Casino',
     'author': "Tejada Tech Group EIRL",
-    'version': '15.0.3.22',
+    'version': '18.0.0.0',
     'sequence': 100,
     'category': 'Accounting/Accounting',
     'description': """
--- models/apuesta_mesa.py
+++ models/apuesta_mesa.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools import float_compare
+# from odoo.tools import float_compare
 import logging
 _logger = logging.getLogger(__name__)
 
