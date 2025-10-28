83672cab53a51eeb9c6a29fa96982944373780d8
Conti <conty1505@gmail.com>
2025-10-28 19:26:29 -0300

    [FIX]Cambios en el manifest

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'TTG - Casino',
     'author': "Tejada Tech Group EIRL",
-    'version': '18.0.0.3',
+    'version': '18.0.0.4',
     'sequence': 100,
     'category': 'Accounting/Accounting',
     'description': """
