84cdc14ee6f9ea3c1bb897e43530f97ea37f4435
Erick Tejada <erick.tejada@tejadatech.com>
2026-04-13 10:35:06 -0400

    activities y list view now hidden by default

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'TTG - Casino',
     'author': "Tejada Tech Group EIRL",
-    'version': '18.0.0.8',
+    'version': '18.0.0.9',
     'sequence': 100,
     'category': 'Accounting/Accounting',
     'description': """
